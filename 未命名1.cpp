f3aefd054243abe6a6f9d6a4d8ab204848d1a920
安权 <88700202+aqsec88@users.noreply.github.com>
2022-01-14 14:44:38 +0800

    算法

---
+++ 未命名1.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*void swap(int *p1, int *p2) 
+ { int temp=*p1;;
+ *p1=*p2;
+ *p2=temp;
+ }
+*/
+int main ()
+{ 
+ int a,b;
+ int *p1=a,*p2=b; 
+//swap (p1,p2);
+ printf("%d %d",*p1,*p2);
+return 0; 
+	
+ } 
+ 
